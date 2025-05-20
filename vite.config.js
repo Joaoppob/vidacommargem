f3536974e89ee0ac495b0ea90565a273262b5fdf
João Pedro Barros <joaoppob@gmail.com>
2025-05-20 16:09:39 -0300

    caminhos

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,24 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import { imagetools } from 'vite-imagetools';
+import { resolve } from 'path';
 
 export default defineConfig({
     base: '/vidacommargem/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                dicas: resolve(__dirname, 'dicas.html'),
+                historias: resolve(__dirname, 'historias.html'),
+                termos: resolve(__dirname, 'termos-de-uso.html'),
+                politica: resolve(__dirname, 'politica-de-privacidade.html'),
+                // Subpastas
+                materia1: resolve(__dirname, 'textos/materia1.html'),
+                crieiUmRitual: resolve(__dirname, 'textos/criei-um-ritual.html'),
+            }
+        }
+    },
     server: {
         port: 3000,
         open: true, // ← Isso aqui abre o navegador automaticamente
